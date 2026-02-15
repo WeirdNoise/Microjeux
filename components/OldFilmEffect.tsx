e3e95ba2482a4a13e457ea702b1079bfe10db071
Weird Noise <contact@weird-noise.com>
2026-02-15 17:39:53 +0100

    feat: Enhance old film effect
    Adjust animation keyframes for Super 8 flicker to a maximum opacity of 0.3 for a more pronounced effect. Add multiple new vertical scratch elements to the `OldFilmEffect` component, varying in width, color, and animation duration/delay to create a more dynamic and realistic film scratch appearance. Add and position several dust/spot elements with subtle blur and varying shapes to enhance the vintage film aesthetic.

--- components/OldFilmEffect.tsx
+++ components/OldFilmEffect.tsx
@@ -45,16 +45,32 @@ const OldFilmEffect: React.FC = () => {
         
         {/* 3. RAYURES VERTICALES (Scratches) */}
         <div className="absolute inset-0 w-full h-full">
-           {/* Rayure 1 */}
+           {/* Rayure 1 (Fine gauche) */}
            <div className="absolute top-0 bottom-0 left-[20%] w-[1px] bg-white/40 h-full animate-scratch" style={{ animationDuration: '3s', animationDelay: '0s' }} />
-           {/* Rayure 2 */}
+           {/* Rayure 2 (Moyenne milieu-droit) */}
            <div className="absolute top-0 bottom-0 left-[65%] w-[2px] bg-black/50 h-full animate-scratch" style={{ animationDuration: '4.5s', animationDelay: '1s' }} />
+           {/* Rayure 3 (Fine droite rapide) */}
+           <div className="absolute top-0 bottom-0 left-[85%] w-[1px] bg-white/30 h-full animate-scratch" style={{ animationDuration: '2.2s', animationDelay: '0.5s' }} />
+           {/* Rayure 4 (Très fine milieu) */}
+           <div className="absolute top-0 bottom-0 left-[45%] w-[1px] bg-black/40 h-full animate-scratch" style={{ animationDuration: '6s', animationDelay: '3s' }} />
+           {/* Rayure 5 (Bord gauche) */}
+           <div className="absolute top-0 bottom-0 left-[5%] w-[1px] bg-white/20 h-full animate-scratch" style={{ animationDuration: '3.5s', animationDelay: '2s' }} />
         </div>
 
         {/* 4. POUSSIÈRES & TÂCHES (Dust) */}
+        {/* Tache ronde floue */}
         <div className="absolute top-[20%] left-[30%] w-2 h-2 rounded-full bg-black/60 blur-[1px] animate-dust" />
+        {/* Cheveu / Fibre longue */}
         <div className="absolute top-[70%] left-[80%] w-24 h-[1px] bg-black/40 rotate-12 animate-dust" style={{ animationDelay: '1.5s' }} />
+        {/* Petite poussière blanche */}
         <div className="absolute top-[50%] left-[10%] w-1 h-3 rounded-full bg-white/40 blur-[1px] animate-dust" style={{ animationDelay: '0.5s' }} />
+        {/* Grosse tache coin haut droit */}
+        <div className="absolute top-[10%] right-[15%] w-4 h-4 rounded-full bg-black/50 blur-[2px] animate-dust" style={{ animationDelay: '2.1s' }} />
+        {/* Fibre verticale bas gauche */}
+        <div className="absolute bottom-[20%] left-[15%] w-[1px] h-16 bg-black/50 -rotate-6 animate-dust" style={{ animationDelay: '0.8s' }} />
+        {/* Poussières microscopiques rapides */}
+        <div className="absolute top-[40%] left-[60%] w-1 h-1 bg-white/50 rounded-full animate-dust" style={{ animationDuration: '0.2s', animationDelay: '4s' }} />
+        <div className="absolute top-[80%] left-[40%] w-1.5 h-1.5 bg-black/40 rounded-full animate-dust" style={{ animationDuration: '0.3s', animationDelay: '1.2s' }} />
 
       </div>
 
